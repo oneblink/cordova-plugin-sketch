dd2f9e49de9ca13d5d2c4dd62c9f270e722ab8fe
Themiya Jayasinghe <jt@blinkmobile.com.au>
2016-04-06 11:57:30 +1000

    Add support for selecting the stroke width and colour

--- src/android/TouchDrawActivity.java
+++ src/android/TouchDrawActivity.java
@@ -18,9 +18,15 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.util.Base64;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -28,6 +34,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TouchDrawActivity extends Activity {
     public static final String DRAWING_RESULT_PARCELABLE = "drawing_result";
@@ -50,6 +58,12 @@ public class TouchDrawActivity extends Activity {
     private String mBackgroundImageUrl = "";
     private Bitmap.CompressFormat mEncodingType = Bitmap.CompressFormat.PNG;
     private int a, r, g, b; //Decoded ARGB color values for the background and erasing
+
+    // Labels and values for stroke colour and width selection buttons
+    private static final String[] STROKE_COLOUR_LABELS = {"RED", "BLUE", "GREEN", "BLACK"};
+    private static final int[] STROKE_COLOUR_VALUES = {Color.RED, Color.BLUE, Color.GREEN, Color.BLACK};
+    private static final String[] STROKE_WIDTH_LABELS = {"0.5x", "1x", "2x", "8x"};
+    private static final Integer[] STROKE_WIDTH_VALUES = {2, 4, 8, 32};
 
     public enum BackgroundImageType {
         DATA_URL,
@@ -83,12 +97,36 @@ public class TouchDrawActivity extends Activity {
             return;
         }
 
-        mTdView = new TouchDrawView(this);
-        LinearLayout tDLayout = new LinearLayout(this);
-        tDLayout.setOrientation(LinearLayout.VERTICAL);
+        RelativeLayout tDLayout = new RelativeLayout(this);
         tDLayout.setHapticFeedbackEnabled(true);
-        tDLayout.addView(createButtonBar());
-        tDLayout.addView(mTdView);
+        tDLayout.setLayoutParams(new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+
+        LinearLayout buttonBar = createButtonBar();
+        buttonBar.setId(getNextViewId());
+        RelativeLayout.LayoutParams buttonBarLayoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        buttonBarLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        buttonBar.setLayoutParams(buttonBarLayoutParams);
+        tDLayout.addView(buttonBar);
+
+        LinearLayout toolBar = createToolBar();
+        toolBar.setId(getNextViewId());
+        RelativeLayout.LayoutParams toolBarLayoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        toolBarLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        toolBar.setLayoutParams(toolBarLayoutParams);
+        tDLayout.addView(toolBar);
+
+        FrameLayout tDContainer = new FrameLayout(this);
+        RelativeLayout.LayoutParams tDViewLayoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        tDViewLayoutParams.addRule(RelativeLayout.BELOW, buttonBar.getId());
+        tDViewLayoutParams.addRule(RelativeLayout.ABOVE, toolBar.getId());
+        tDContainer.setLayoutParams(tDViewLayoutParams);
+        mTdView = new TouchDrawView(this);
+        tDContainer.addView(mTdView);
+        tDLayout.addView(tDContainer);
 
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(tDLayout);
@@ -105,7 +143,6 @@ public class TouchDrawActivity extends Activity {
 
     public LinearLayout createButtonBar() {
         LinearLayout buttonBar = new LinearLayout(this);
-        buttonBar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 
         Button doneButton = new Button(this);
         doneButton.setText("Done");
@@ -121,7 +158,6 @@ public class TouchDrawActivity extends Activity {
 
         Button eraseButton = new Button(this);
         eraseButton.setText("Erase");
-
         eraseButton.setBackgroundColor(Color.GRAY);
         eraseButton.setLayoutParams(new LinearLayout.LayoutParams(
                 0, ViewGroup.LayoutParams.MATCH_PARENT, (float) 0.30));
@@ -134,7 +170,6 @@ public class TouchDrawActivity extends Activity {
 
         Button cancelButton = new Button(this);
         cancelButton.setText("Cancel");
-
         cancelButton.setBackgroundColor(Color.RED);
         cancelButton.setLayoutParams(new LinearLayout.LayoutParams(
                 0, ViewGroup.LayoutParams.MATCH_PARENT, (float) 0.30));
@@ -151,6 +186,114 @@ public class TouchDrawActivity extends Activity {
         buttonBar.addView(cancelButton);
 
         return buttonBar;
+    }
+
+    public LinearLayout createToolBar() {
+        LinearLayout toolBar = new LinearLayout(this);
+
+        toolBar.addView(createColourSpinner());
+        toolBar.addView(createWidthSpinner());
+
+        return toolBar;
+    }
+
+    public Spinner createColourSpinner() {
+        final String strokeColourLabelPrefix = "COLOUR: ";
+        Spinner spinner = new Spinner(this);
+
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
+                android.R.layout.simple_spinner_dropdown_item, STROKE_COLOUR_LABELS) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                TextView v = (TextView) super.getView(position, convertView, parent);
+
+                v.setText(strokeColourLabelPrefix + "BLUE");
+                return v;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                TextView v = (TextView) super.getDropDownView(position, convertView,parent);
+
+                v.setText(STROKE_COLOUR_LABELS[position]);
+                return v;
+            }
+        };
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                if (mPaint != null && position >= 0 && position < STROKE_COLOUR_VALUES.length) {
+                    mPaint.setColor(STROKE_COLOUR_VALUES[position]);
+
+                    adapterView.setBackgroundColor(STROKE_COLOUR_VALUES[position]);
+                    ((TextView) view).setText(strokeColourLabelPrefix + STROKE_COLOUR_LABELS[position]);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        spinner.setBackgroundColor(Color.BLUE);
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                0, ViewGroup.LayoutParams.MATCH_PARENT, (float) 0.30);
+        spinner.setLayoutParams(layoutParams);
+        spinner.setSelection(Arrays.asList(STROKE_COLOUR_LABELS).indexOf("BLUE"));
+
+        return spinner;
+    }
+
+    public Spinner createWidthSpinner() {
+        final String strokeWidthLabelPrefix = "WIDTH: ";
+        Spinner spinner = new Spinner(this);
+
+        final ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
+                android.R.layout.simple_spinner_dropdown_item, STROKE_WIDTH_LABELS) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                TextView v = (TextView) super.getView(position, convertView, parent);
+
+                v.setText(strokeWidthLabelPrefix +
+                        STROKE_WIDTH_LABELS[Arrays.asList(STROKE_WIDTH_VALUES).indexOf(mStrokeWidth)]);
+                return v;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                TextView v = (TextView) super.getDropDownView(position, convertView,parent);
+
+                v.setText(STROKE_WIDTH_LABELS[position]);
+                return v;
+            }
+        };
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                mStrokeWidth = STROKE_WIDTH_VALUES[position];
+                ((TextView) view).setText(strokeWidthLabelPrefix +
+                        STROKE_WIDTH_LABELS[Arrays.asList(STROKE_WIDTH_VALUES).indexOf(mStrokeWidth)]);
+
+                if (mPaint != null) {
+                    mPaint.setStrokeWidth(mStrokeWidth);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                0, ViewGroup.LayoutParams.MATCH_PARENT, (float) 0.30);
+        spinner.setLayoutParams(layoutParams);
+        spinner.setSelection(Arrays.asList(STROKE_WIDTH_LABELS).indexOf("1x"));
+
+        return spinner;
     }
 
     public void eraseDrawing() {
@@ -428,4 +571,23 @@ public class TouchDrawActivity extends Activity {
         TouchDrawActivity.this.setResult(RESULT_TOUCHDRAW_ERROR, result);
         TouchDrawActivity.this.finish();
     }
+
+    private int getNextViewId() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            return View.generateViewId(); // Added in API level 17
+        }
+
+        // Re-implement View.generateViewId()for API levels < 17
+        // http://stackoverflow.com/a/15442898
+        for (;;) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
+    }
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
 }
